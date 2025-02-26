42ef83326548732f284b341bc8e6c339252f709f
AGYAPAL SINGH <agyapalsinghaujla@gmail.com>
2025-02-26 19:47:08 +0530

    Navigation Bar Created

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,7 +1,60 @@
 import React from "react";
+import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+import { BiBook } from "react-icons/bi";
+import { SiBookstack } from "react-icons/si";
+import { BiMessageSquareDetail } from "react-icons/bi";
+import { useState } from "react";
+import { MdWorkOutline } from "react-icons/md";
 
-const Navigation = () => {
-  return <div>Navigation</div>;
+const Navgation = () => {
+  const [activeNav, setActiveNav] = useState("#");
+  return (
+    <nav>
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <AiOutlineHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <AiOutlineUser />
+      </a>
+      <a
+        href="#education"
+        onClick={() => setActiveNav("#education")}
+        className={activeNav === "#education" ? "active" : ""}
+      >
+        <BiBook />
+      </a>
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
+        <SiBookstack />
+      </a>
+      <a
+        href="#project"
+        onClick={() => setActiveNav("#project")}
+        className={activeNav === "#project" ? "active" : ""}
+      >
+        <MdWorkOutline />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <BiMessageSquareDetail />
+      </a>
+    </nav>
+  );
 };
 
-export default Navigation;
+export default Navgation;
